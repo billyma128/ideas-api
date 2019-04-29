0abe3bd07042ba4fe8d79f21480869a069540f73
Billy Ma <billyma128@gmail.com>
2019-04-30 01:05:15 +0800

    Added authentication

---
+++ src/shared/auth.guard.ts
@@ -0,0 +1,36 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  HttpException,
+  HttpStatus,
+  Injectable,
+} from '@nestjs/common';
+import * as jwt from 'jsonwebtoken';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const auth = request.headers.authorization;
+    if (!auth) {
+      return false;
+    }
+
+    request.user = await this.validateToken(auth);
+    return true;
+  }
+
+  async validateToken(auth: string) {
+    if (auth.split(' ')[0] !== 'Bearer') {
+      throw new HttpException('Invalid token ', HttpStatus.FORBIDDEN);
+    }
+    const token = auth.split(' ')[1];
+    try {
+      const decoded = jwt.verify(token, process.env.SECRET);
+      return decoded;
+    } catch (err) {
+      const message = `Token error: ${err.message || err.name}`;
+      throw new HttpException(message, HttpStatus.FORBIDDEN);
+    }
+  }
+}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,5 +1,14 @@
-import { Body, Controller, Get, Post, UsePipes } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UseGuards,
+  UsePipes,
+} from '@nestjs/common';
+import { AuthGuard } from '../shared/auth.guard';
 import { ValidationPipe } from '../shared/validation.pipe';
+import { User } from './user.decorator';
 import { UserDTO } from './user.dto';
 import { UserService } from './user.service';
 
@@ -8,7 +17,9 @@ export class UserController {
   constructor(private userService: UserService) {}
 
   @Get('api/users')
-  showAllUsers() {
+  @UseGuards(new AuthGuard())
+  showAllUsers(@User() user) {
+    console.log(user);
     return this.userService.showAll();
   }
 
